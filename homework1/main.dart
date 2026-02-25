ac1724d9c969fd8c59303e9c69f5e7661da240a4
Mohammed Alharbi <445156383@tvtc.edu.sa>
2026-02-25 15:50:17 +0300

    Add HTML document with personal information

---
+++ homework1/main.dart
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <title>Document</title>
+</head>
+<body>
+   <p style="text-align: center; margin: 110px;">Mohammed Eid Alharbi</p> 
+   <p style="text-align: center; margin: 20px;">445156383</p>
+</body>
+</html>
+
