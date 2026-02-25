a11f060275edd00dddd017fbc19576c2e519a5d3
Mohammed Alharbi <445156383@tvtc.edu.sa>
2026-02-25 15:27:19 +0300

    Update main.dart

--- homework2/main.dart
+++ homework2/main.dart
@@ -1,1 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Resume - Start Bootstrap Theme</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">محمد عيد الحربي</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/im1.jpg" alt="..." /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">عن</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">الخبرة</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">التعليم</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">المهارات</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">الأهتمامات</a></li>
+                </ul>
+            </div>
+        </nav>
 
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+            <!-- About-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-5">
+                       محمد عيد
+                        <span class="text-primary">الحربي</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                      مدينة الرياض , المملكة العربية السعودية
+                        <a href="mailto:name@email.com">zfjplu@email.com</a>
+                    </div>
+                    <p class="lead mb-5"> لدي خبرة في استخدام أطر العمل المرنة لتقديم ملخصات شاملة ودقيقة. وتساهم المناهج التكرارية في استراتيجية الشركات في تعزيز
+                        التفكير التعاوني لزيادة القيمة الإجمالية للعرض. </p>
+                    <div class="social-icons">
+                        <a class="social-icon" href="#!"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="social-icon" href="#!"><i class="fab fa-github"></i></a>
+                        <a class="social-icon" href="#!"><i class="fab fa-twitter"></i></a>
+                        <a class="social-icon" href="#!"><i class="fab fa-facebook-f"></i></a>
+                    </div>
+                </div>
+            </section>
+
+
+            <hr class="m-0" />
+
+            <!-- الخبرة -->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">الخبرة</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        
+                        <div class="flex-shrink-0"><span class="text-primary">فبراير 2026 - حتى الآن</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">مصمم مواقع ويب مبتدئ</h3>
+                            <div class="subheading mb-3">شركة شوت  للأنتاج العالمي</div>
+                            <p>دارة التغيير التشغيلي عبر البودكاست ضمن سير العمل لوضع إطار عمل. نقل مؤشرات الأداء الرئيسية السلسة إلى وضع عدم الاتصال بالإنترنت لتحقيق أقصى استفادة من الموارد المتاحة. التركيز على الهدف مع التعمق في عقلية الشركات الناشئة للوصول إلى تكامل سلس بين المنصات.
+                           </p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">يوليو 2024 - فبراير 2026</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">متدرب في تصميم المواقع الألكترونية</h3>
+                            <div class="subheading mb-3">شركة شوت للأنتاج العالمي</div>
+                            <p> إدارة الأسواق المُمكّنة بشكل تعاوني عبر شبكات سهلة الاستخدام. تأجيل تفاعل مستخدمي B2C بشكل ديناميكي بعد الاستفادة من قاعدة المستخدمين المُثبّتين. تصوّر التقارب الموجّه نحو العملاء بشكل جذري دون عائد استثمار ثوري.
+                           </p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">سبتمبر 2023 - يونيو 2024</span></div>
+                    </div>
+                </div>
+            </section>
+
+
+            <hr class="m-0" />
+
+
+            <!-- التعليم-->
+            <section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">التعليم</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">كلية الأتصالات والمعلومات التنقية بالرياض</h3>
+                            <div class="subheading mb-3">دبلوم متوسط في علوم الحاسب</div>
+                            <div>علوم الحاسب- مسار تطوير الويب</div>
+                            <p>المعدل التراكمي : 3.23</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">اغسطس 2024 - فبراير2026 </span></div>
+                    </div>
+                   
+                </div>
+            </section>
+
+            <hr class="m-0" />
+
+
+            <!-- المهارات -->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">المهارات</h2>
+                    <div class="subheading mb-3">لغات وأدوات البرمجة</div>
+                    <ul class="list-inline dev-icons">
+                        <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-angular"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-react"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-node-js"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-sass"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-less"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-gulp"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-grunt"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-npm"></i></li>
+                    </ul>
+                
+
+
+                                </div>
+                            </section>
+
+
+                            <hr class="m-0" />
+
+                            <!--  الاهتمامات -->
+                            <section class="resume-section" id="interests">
+                                <div class="resume-section-content">
+                                    <h2 class="mb-5">الأهتمامات</h2>
+                                    <p>الى جانب عملي كمطور مواقع إلكترونية، أستمتع بقضاء معظم وقتي في الهواء الطلق. في الشتاء، أنا متزلج شغوف ومتسلق جليد مبتدئ. أما خلال الأشهر الدافئة هنا في كولورادو، فأستمتع بركوب الدراجات الجبلية والتسلق الحر والتجديف.</p>
+                                    <p class="mb-0">عندما أضطر للبقاء في المنزل، أتابع عدداً من أفلام ومسلسلات الخيال العلمي والفانتازيا، وأنا طاهٍ طموح، وأقضي جزءاً كبيراً من وقت فراغي في استكشاف أحدث التطورات التكنولوجية في عالم تطوير واجهات المواقع الإلكترونية.</p>
+                                </div>
+                            </section>
+
+                            <hr class="m-0" />
+
+                 
+            
+        </div>
+
+
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
+ 
